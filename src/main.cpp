e3500cbd443234edc224caf3289e342c7ff2c9c1
Stepan Salenikovich <stepan.salenikovich@savoirfairelinux.com>
2015-02-13 15:55:46 -0500

    gnome: CMakeLists for new client
    A simple main.cpp to test the cmake config
    and make sure libRingClient starts in a GTK
    main loop.
    
    Refs #66150
    
    Change-Id: I85ca9e984f77d0f13d25e3bd9361885cf8eb01d4

---
+++ src/main.cpp
@@ -0,0 +1,67 @@
+/*
+ *  Copyright (C) 2004-2015 Savoir-Faire Linux Inc.
+ *  Author: Stepan Salenikovich <stepan.salenikovich@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  If you modify this program, or any covered work, by linking or
+ *  combining it with the OpenSSL project's OpenSSL library (or a
+ *  modified version of that library), containing parts covered by the
+ *  terms of the OpenSSL or SSLeay licenses, Savoir-Faire Linux Inc.
+ *  grants you additional permission to convey the resulting work.
+ *  Corresponding Source for a non-source form of such a combination
+ *  shall include the source code for the parts of OpenSSL used as well
+ *  as that of the covered work.
+ */
+
+#include <gtk/gtk.h>
+#include <QCoreApplication>
+#include <callmodel.h>
+
+int
+main(int argc, char *argv[])
+{
+    GtkWidget *window;
+
+    gtk_init (&argc, &argv);
+
+    // enable debug
+    g_setenv("G_MESSAGES_DEBUG", "all", TRUE);
+    g_debug("debug enabled");
+
+    QCoreApplication *app = NULL;
+    try {
+        app = new QCoreApplication(argc, argv);
+        CallModel::instance();
+    } catch (const char * msg) {
+        g_debug("caught error: %s\n", msg);
+        return 1;
+    }
+
+    // create an empty window
+    window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title (GTK_WINDOW (window), "Ring");
+
+    g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
+
+    gtk_widget_show (window);
+
+    gtk_main ();
+
+    delete app;
+    return 0;
+}
