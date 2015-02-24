38266436f08f68d6f153bf2a295760f9451417c3
Stepan Salenikovich <stepan.salenikovich@savoirfairelinux.com>
2015-02-24 18:11:45 -0500

    gnome: incoming call view
    Can now accept or reject incoming calls.
    
    Refs #66962
    Refs #67159
    
    Change-Id: Ia68a37efdd71735623e7ef013092450102426d68

--- src/ringmainwindow.cpp
+++ src/ringmainwindow.cpp
@@ -33,6 +33,11 @@
 #include <gtk/gtk.h>
 #include "models/gtkqtreemodel.h"
 #include <callmodel.h>
+#include <call.h>
+#include <QItemSelectionModel>
+#include "incomingcallview.h"
+
+#define DEFAULT_VIEW_NAME "placeholder"
 
 struct _RingMainWindow
 {
@@ -52,11 +57,101 @@ struct _RingMainWindowPrivate
     GtkWidget *gears_image;
     GtkWidget *treeview_call;
     GtkWidget *search_entry;
+    GtkWidget *stack_main_view;
 };
 
 G_DEFINE_TYPE_WITH_PRIVATE(RingMainWindow, ring_main_window, GTK_TYPE_APPLICATION_WINDOW);
 
 #define RING_MAIN_WINDOW_GET_PRIVATE(obj) (G_TYPE_INSTANCE_GET_PRIVATE ((obj), RING_MAIN_WINDOW_TYPE, RingMainWindowPrivate))
+
+static QModelIndex
+get_index_from_selection(GtkTreeSelection *selection)
+{
+    GtkTreeIter iter;
+    GtkTreeModel *model = NULL;
+
+    if (gtk_tree_selection_get_selected(selection, &model, &iter)) {
+        return gtk_q_tree_model_get_source_idx(GTK_Q_TREE_MODEL(model), &iter);
+    } else {
+        return QModelIndex();
+    }
+}
+
+static void
+update_call_model_selection(GtkTreeSelection *selection, gpointer win)
+{
+    RingMainWindowPrivate *priv = RING_MAIN_WINDOW_GET_PRIVATE(RING_MAIN_WINDOW(win));
+    GtkQTreeModel *call_model = GTK_Q_TREE_MODEL(gtk_tree_view_get_model(GTK_TREE_VIEW(priv->treeview_call)));
+    CallModel *call_qmodel = (CallModel *)gtk_q_tree_model_get_qmodel(call_model);
+    QModelIndex current = get_index_from_selection(selection);
+    if (current.isValid())
+        call_qmodel->selectionModel()->setCurrentIndex(current, QItemSelectionModel::ClearAndSelect);
+    else
+       call_qmodel->selectionModel()->clearCurrentIndex();
+}
+
+static void
+call_selection_changed(GtkTreeSelection *selection, gpointer win)
+{
+    RingMainWindowPrivate *priv = RING_MAIN_WINDOW_GET_PRIVATE(RING_MAIN_WINDOW(win));
+
+    /* get the current visible stack child */
+    GtkWidget *child_old = gtk_stack_get_visible_child(GTK_STACK(priv->stack_main_view));
+
+    QModelIndex idx = get_index_from_selection(selection);
+    if (idx.isValid()) {
+        QVariant state = idx.model()->data(idx, Call::Role::CallState);
+
+        switch(state.value<Call::State>()) {
+            case Call::State::INCOMING:
+            {
+                QVariant name = idx.model()->data(idx, Call::Role::Name);
+                QByteArray ba_name = name.toString().toLocal8Bit();
+                GtkWidget *new_call_view = incoming_call_view_new();
+                incoming_call_view_set_call_info(INCOMING_CALL_VIEW(new_call_view), idx);
+                gtk_stack_add_named(GTK_STACK(priv->stack_main_view), new_call_view, ba_name.data());
+                gtk_stack_set_visible_child(GTK_STACK(priv->stack_main_view), new_call_view);
+            }
+            break;
+
+            /* TODO: implement view(s) for other call states */
+            case Call::State::RINGING:
+            case Call::State::CURRENT:
+            case Call::State::DIALING:
+            case Call::State::HOLD:
+            case Call::State::FAILURE:
+            case Call::State::BUSY:
+            case Call::State::TRANSFERRED:
+            case Call::State::TRANSF_HOLD:
+            case Call::State::OVER:
+            case Call::State::ERROR:
+            case Call::State::CONFERENCE:
+            case Call::State::CONFERENCE_HOLD:
+            case Call::State::INITIALIZATION:
+            case Call::State::COUNT__:
+            break;
+        }
+    } else {
+        /* nothing selected in the call model, so show the default screen */
+
+        /* TODO: replace stack paceholder view */
+        gtk_stack_set_transition_type(GTK_STACK(priv->stack_main_view), GTK_STACK_TRANSITION_TYPE_SLIDE_LEFT);
+        gtk_stack_set_visible_child_name(GTK_STACK(priv->stack_main_view), DEFAULT_VIEW_NAME);
+        gtk_stack_set_transition_type(GTK_STACK(priv->stack_main_view), GTK_STACK_TRANSITION_TYPE_SLIDE_RIGHT);
+
+    }
+
+     /* check if we changed the visible child */
+    GtkWidget *child_current = gtk_stack_get_visible_child(GTK_STACK(priv->stack_main_view));
+    if (child_current != child_old && child_old != NULL) {
+        /* if the previous child was an "incoming call", then remove it from
+         * the stack; removing it should destory it since there should not
+         * be any other references to it */
+        if (IS_INCOMING_CALL_VIEW(child_old)) {
+            gtk_container_remove(GTK_CONTAINER(priv->stack_main_view), child_old);
+        }
+    }
+}
 
 static void
 ring_main_window_init(RingMainWindow *win)
@@ -89,32 +184,67 @@ ring_main_window_init(RingMainWindow *win)
     g_object_unref(builder);
 
     /* call model */
-    GtkQTreeModel *model;
+    GtkQTreeModel *call_model;
     GtkCellRenderer *renderer;
     GtkTreeViewColumn *column;
 
-    model = gtk_q_tree_model_new(CallModel::instance(), 4,
+    call_model = gtk_q_tree_model_new(CallModel::instance(), 4,
         Call::Role::Name, G_TYPE_STRING,
         Call::Role::Number, G_TYPE_STRING,
         Call::Role::Length, G_TYPE_STRING,
         Call::Role::CallState, G_TYPE_STRING);
-    gtk_tree_view_set_model(GTK_TREE_VIEW(priv->treeview_call), GTK_TREE_MODEL(model) );
+    gtk_tree_view_set_model(GTK_TREE_VIEW(priv->treeview_call), GTK_TREE_MODEL(call_model));
 
     renderer = gtk_cell_renderer_text_new();
     column = gtk_tree_view_column_new_with_attributes("Name", renderer, "text", 0, NULL);
-    gtk_tree_view_append_column(GTK_TREE_VIEW (priv->treeview_call), column);
-
-    renderer = gtk_cell_renderer_text_new();
-    column = gtk_tree_view_column_new_with_attributes("Number", renderer, "text", 1, NULL);
     gtk_tree_view_append_column(GTK_TREE_VIEW(priv->treeview_call), column);
 
     renderer = gtk_cell_renderer_text_new();
     column = gtk_tree_view_column_new_with_attributes("Duration", renderer, "text", 2, NULL);
     gtk_tree_view_append_column(GTK_TREE_VIEW(priv->treeview_call), column);
 
-    renderer = gtk_cell_renderer_text_new();
-    column = gtk_tree_view_column_new_with_attributes("State", renderer, "text", 3, NULL);
-    gtk_tree_view_append_column(GTK_TREE_VIEW(priv->treeview_call), column);
+    /* connect signals to and from UserActionModel to sync selection betwee
+     * the QModel and the GtkTreeView */
+    CallModel *call_qmodel = (CallModel *)gtk_q_tree_model_get_qmodel(call_model);
+
+    QObject::connect(
+        call_qmodel->selectionModel(),
+        &QItemSelectionModel::currentChanged,
+        [=](const QModelIndex & current, const QModelIndex & previous) {
+            GtkTreeSelection *selection = gtk_tree_view_get_selection(GTK_TREE_VIEW(priv->treeview_call));
+
+            /* first unselect the previous */
+            if (previous.isValid()) {
+                GtkTreeIter old_iter;
+                if (gtk_q_tree_model_source_index_to_iter(call_model, previous, &old_iter)) {
+                    gtk_tree_selection_unselect_iter(selection, &old_iter);
+                } else {
+                    g_warning("Trying to unselect invalid GtkTreeIter");
+                }
+            }
+
+            /* select the current */
+            if (current.isValid()) {
+                GtkTreeIter new_iter;
+                if (gtk_q_tree_model_source_index_to_iter(call_model, current, &new_iter)) {
+                    gtk_tree_selection_select_iter(selection, &new_iter);
+                } else {
+                    g_warning("SelectionModel of CallModel changed to invalid QModelIndex?");
+                }
+            }
+        }
+    );
+
+    GtkTreeSelection *call_selection = gtk_tree_view_get_selection(GTK_TREE_VIEW(priv->treeview_call));
+    g_signal_connect(call_selection, "changed", G_CALLBACK(update_call_model_selection), win);
+
+    /* TODO: replace stack paceholder view */
+    GtkWidget *placeholder_view = gtk_tree_view_new();
+    gtk_widget_show(placeholder_view);
+    gtk_stack_add_named(GTK_STACK(priv->stack_main_view), placeholder_view, DEFAULT_VIEW_NAME);
+
+    /* connect signals */
+    g_signal_connect(call_selection, "changed", G_CALLBACK(call_selection_changed), win);
 }
 
 static void
@@ -127,6 +257,7 @@ ring_main_window_class_init(RingMainWindowClass *klass)
     gtk_widget_class_bind_template_child_private(GTK_WIDGET_CLASS (klass), RingMainWindow, gears);
     gtk_widget_class_bind_template_child_private(GTK_WIDGET_CLASS (klass), RingMainWindow, gears_image);
     gtk_widget_class_bind_template_child_private(GTK_WIDGET_CLASS (klass), RingMainWindow, search_entry);
+    gtk_widget_class_bind_template_child_private(GTK_WIDGET_CLASS (klass), RingMainWindow, stack_main_view);
 }
 
 GtkWidget *
