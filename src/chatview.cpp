114c9bebfae420a56a0f4e14e75b1af5da806ca7
Stepan Salenikovich <stepan.salenikovich@savoirfairelinux.com>
2016-01-08 13:43:49 -0500

    WIP chat view
    Change-Id: I18a339b4b6b4bc34b415e9f60d25884485d97c36

---
+++ src/chatview.cpp
@@ -0,0 +1,255 @@
+/*
+ *  Copyright (C) 2016 Savoir-faire Linux Inc.
+ *  Author: Stepan Salenikovich <stepan.salenikovich@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+#include "chatview.h"
+
+#include <gtk/gtk.h>
+#include <call.h>
+#include <callmodel.h>
+#include <contactmethod.h>
+#include <person.h>
+#include <media/media.h>
+#include <media/text.h>
+#include <media/textrecording.h>
+#include "ringnotify.h"
+
+struct _ChatView
+{
+    GtkBox parent;
+};
+
+struct _ChatViewClass
+{
+    GtkBoxClass parent_class;
+};
+
+typedef struct _ChatViewPrivate ChatViewPrivate;
+
+struct _ChatViewPrivate
+{
+    GtkWidget *textview_chat;
+    GtkWidget *button_chat_input;
+    GtkWidget *entry_chat_input;
+    GtkWidget *scrolledwindow_chat;
+
+    Call *call;
+
+    QMetaObject::Connection media_added_connection;
+    QMetaObject::Connection new_message_connection;
+    QMetaObject::Connection incoming_msg_connection;
+};
+
+G_DEFINE_TYPE_WITH_PRIVATE(ChatView, chat_view, GTK_TYPE_BOX);
+
+#define CHAT_VIEW_GET_PRIVATE(obj) (G_TYPE_INSTANCE_GET_PRIVATE ((obj), CHAT_VIEW_TYPE, ChatViewPrivate))
+
+enum {
+    NEW_MESSAGES_DISPLAYED,
+    LAST_SIGNAL
+};
+
+static guint chat_view_signals[LAST_SIGNAL] = { 0 };
+
+static void
+chat_view_dispose(GObject *object)
+{
+    ChatView *view;
+    ChatViewPrivate *priv;
+
+    view = CHAT_VIEW(object);
+    priv = CHAT_VIEW_GET_PRIVATE(view);
+
+    QObject::disconnect(priv->media_added_connection);
+    QObject::disconnect(priv->new_message_connection);
+    QObject::disconnect(priv->incoming_msg_connection);
+
+    G_OBJECT_CLASS(chat_view_parent_class)->dispose(object);
+}
+
+
+static void
+send_chat(G_GNUC_UNUSED GtkWidget *widget, ChatView *self)
+{
+    g_return_if_fail(IS_CHAT_VIEW(self));
+    ChatViewPrivate *priv = CHAT_VIEW_GET_PRIVATE(self);
+
+    /* make sure there is text to send */
+    const gchar *text = gtk_entry_get_text(GTK_ENTRY(priv->entry_chat_input));
+    if (text && strlen(text) > 0) {
+        QMap<QString, QString> messages;
+        messages["text/plain"] = text;
+        priv->call->addOutgoingMedia<Media::Text>()->send(messages);
+        /* clear the entry */
+        gtk_entry_set_text(GTK_ENTRY(priv->entry_chat_input), "");
+    }
+}
+
+static void
+scroll_to_bottom(GtkAdjustment *adjustment, G_GNUC_UNUSED gpointer user_data)
+{
+    gtk_adjustment_set_value(adjustment,
+        gtk_adjustment_get_upper(adjustment) - gtk_adjustment_get_page_size(adjustment));
+}
+
+static void
+chat_view_init(ChatView *view)
+{
+    gtk_widget_init_template(GTK_WIDGET(view));
+
+    ChatViewPrivate *priv = CHAT_VIEW_GET_PRIVATE(view);
+
+    g_signal_connect(priv->button_chat_input, "clicked", G_CALLBACK(send_chat), view);
+    g_signal_connect(priv->entry_chat_input, "activate", G_CALLBACK(send_chat), view);
+}
+
+static void
+chat_view_class_init(ChatViewClass *klass)
+{
+    G_OBJECT_CLASS(klass)->dispose = chat_view_dispose;
+
+    gtk_widget_class_set_template_from_resource(GTK_WIDGET_CLASS (klass),
+                                                "/cx/ring/RingGnome/chatview.ui");
+
+    gtk_widget_class_bind_template_child_private(GTK_WIDGET_CLASS (klass), ChatView, textview_chat);
+    gtk_widget_class_bind_template_child_private(GTK_WIDGET_CLASS (klass), ChatView, button_chat_input);
+    gtk_widget_class_bind_template_child_private(GTK_WIDGET_CLASS (klass), ChatView, entry_chat_input);
+    gtk_widget_class_bind_template_child_private(GTK_WIDGET_CLASS (klass), ChatView, scrolledwindow_chat);
+
+    chat_view_signals[NEW_MESSAGES_DISPLAYED] = g_signal_new (
+        "new-messages-displayed",
+        G_TYPE_FROM_CLASS(klass),
+        (GSignalFlags) (G_SIGNAL_RUN_FIRST | G_SIGNAL_ACTION),
+        0,
+        nullptr,
+        nullptr,
+        g_cclosure_marshal_VOID__VOID,
+        G_TYPE_NONE, 0);
+}
+
+static void
+print_message_to_buffer(const QModelIndex &idx, GtkTextBuffer *buffer)
+{
+    if (idx.isValid()) {
+        auto message = idx.data().value<QString>().toUtf8();
+        auto sender = idx.data(static_cast<int>(Media::TextRecording::Role::AuthorDisplayname)).value<QString>().toUtf8();
+
+        GtkTextIter iter;
+
+        /* unless its the very first message, insert a new line */
+        if (idx.row() != 0) {
+            gtk_text_buffer_get_end_iter(buffer, &iter);
+            gtk_text_buffer_insert(buffer, &iter, "\n", -1);
+        }
+
+        auto format_sender = g_strconcat(sender.constData(), ": ", NULL);
+        gtk_text_buffer_get_end_iter(buffer, &iter);
+        gtk_text_buffer_insert_with_tags_by_name(buffer, &iter,
+                                                 format_sender, -1,
+                                                 "bold", NULL);
+        g_free(format_sender);
+
+        /* if the sender name is too long, insert a new line after it */
+        if (sender.length() > 20) {
+            gtk_text_buffer_get_end_iter(buffer, &iter);
+            gtk_text_buffer_insert(buffer, &iter, "\n", -1);
+        }
+
+        gtk_text_buffer_get_end_iter(buffer, &iter);
+        gtk_text_buffer_insert(buffer, &iter, message.constData(), -1);
+
+    } else {
+        g_warning("QModelIndex in im model is not valid");
+    }
+}
+
+static void
+parse_chat_model(QAbstractItemModel *model, ChatView *self)
+{
+    g_return_if_fail(IS_CHAT_VIEW(self));
+    ChatViewPrivate *priv = CHAT_VIEW_GET_PRIVATE(self);
+
+    /* new model, disconnect from the old model updates and clear the text buffer */
+    QObject::disconnect(priv->new_message_connection);
+
+    GtkTextBuffer *new_buffer = gtk_text_buffer_new(NULL);
+    gtk_text_view_set_buffer(GTK_TEXT_VIEW(priv->textview_chat), new_buffer);
+
+    /* add tags to the buffer */
+    gtk_text_buffer_create_tag(new_buffer, "bold", "weight", PANGO_WEIGHT_BOLD, NULL);
+
+    g_object_unref(new_buffer);
+
+    /* put all the messages in the im model into the text view */
+    for (int row = 0; row < model->rowCount(); ++row) {
+        QModelIndex idx = model->index(row, 0);
+        print_message_to_buffer(idx, new_buffer);
+    }
+
+    /* append new messages */
+    priv->new_message_connection = QObject::connect(
+        model,
+        &QAbstractItemModel::rowsInserted,
+        [priv, model] (const QModelIndex &parent, int first, int last) {
+            for (int row = first; row <= last; ++row) {
+                QModelIndex idx = model->index(row, 0, parent);
+                print_message_to_buffer(idx, gtk_text_view_get_buffer(GTK_TEXT_VIEW(priv->textview_chat)));
+                g_signal_emit(G_OBJECT(view), chat_view_signals[NEW_MESSAGES_DISPLAYED], 0);
+            }
+        }
+    );
+}
+
+void
+chat_view_set_call_info(ChatView *view, const QModelIndex& idx) {
+    ChatViewPrivate *priv = CHAT_VIEW_GET_PRIVATE(view);
+
+    priv->call = CallModel::instance().getCall(idx);
+
+
+    /* check if text media is already present */
+    if (priv->call->hasMedia(Media::Media::Type::TEXT, Media::Media::Direction::IN)) {
+        Media::Text *text = priv->call->firstMedia<Media::Text>(Media::Media::Direction::IN);
+        parse_chat_model(text->recording()->instantMessagingModel(), view);
+    } else if (priv->call->hasMedia(Media::Media::Type::TEXT, Media::Media::Direction::OUT)) {
+        Media::Text *text = priv->call->firstMedia<Media::Text>(Media::Media::Direction::OUT);
+        parse_chat_model(text->recording()->instantMessagingModel(), view);
+    } else {
+        /* monitor media for messaging text messaging */
+        priv->media_added_connection = QObject::connect(
+            priv->call,
+            &Call::mediaAdded,
+            [view, priv] (Media::Media* media) {
+                if (media->type() == Media::Media::Type::TEXT) {
+                    parse_chat_model(((Media::Text*)media)->recording()->instantMessagingModel(), view);
+                    QObject::disconnect(priv->media_added_connection);
+                }
+            }
+        );
+    }
+
+    /* check if there were any chat notifications and open the chat view if so */
+    if (ring_notify_close_chat_notification(priv->call))
+        g_signal_emit(G_OBJECT(view), chat_view_signals[NEW_MESSAGES_DISPLAYED], 0);
+}
+
+GtkWidget *
+chat_view_new(void)
+{
+    return (GtkWidget *)g_object_new(CHAT_VIEW_TYPE, NULL);
+}
