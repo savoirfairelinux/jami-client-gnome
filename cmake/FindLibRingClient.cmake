1a5a3711d46f511fb85faa6210162dc341ac4366
aviau <alexandre@alexandreviau.net>
2016-03-18 15:19:02 -0400

    Allow building the client without installing lrc
    Change-Id: I580eebff234be91abb93ed2f12941e924afca109

---
+++ cmake/FindLibRingClient.cmake
@@ -0,0 +1,13 @@
+# Once done this will define
+#  LibRingClient_INCLUDE_DIRS - include directories
+#  LibRingClient_LIBRARY_DIRS - library directories
+#  LIB_RING_CLIENT_LIBRARY - library name
+
+IF(EXISTS ${LibRingClient_DIR}/src/callmodel.h)
+   SET(LibRingClient_FOUND true)
+   SET(LibRingClient_INCLUDE_DIRS ${LibRingClient_DIR}/src)
+   SET(LibRingClient_LIBRARY_DIRS ${LibRingClient_DIR}/build)
+   SET(LIB_RING_CLIENT_LIBRARY ringclient)
+   MESSAGE("lrc headers are in " ${LibRingClient_INCLUDE_DIRS})
+   MESSAGE("lrc libraries are in " ${LibRingClient_LIBRARY_DIRS})
+ENDIF()
