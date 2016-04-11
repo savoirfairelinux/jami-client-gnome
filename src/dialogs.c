4a664bd014d23b49003a05cc910d8f114cfaabce
experimentella <etcetin@gmail.com>
2016-04-11 21:18:57 +0300

    Update dialog.c
    Static value changed to dynamic value which can be read from language file.

--- src/dialogs.c
+++ src/dialogs.c
@@ -116,7 +116,15 @@ ring_about_dialog(GtkWidget *parent)
         "website", "http://www.ring.cx/",
         "website-label", "www.ring.cx",
         "artists", artists,
-        "translator-credits", "https://www.transifex.com/savoirfairelinux/ring",
+        /* Translators: This is a special message that shouldn't be translated
+        * literally. It is used in the about box to give credits to
+        * the translators.
+        * Thus, you should translate it to your name and email address.
+        * You should also include other translators who have contributed to
+        * this translation; in that case, please write each of them on a separate
+        * line seperated by newlines (\n).
+        */
+        "translator-credits", _("translator-credits"),
         NULL
     );
 
