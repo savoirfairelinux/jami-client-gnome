3781b6925e8b337681cbfe44cf86da7dad610983
Nicolas Jager <nicolas.jager@savoirfairelinux.com>
2016-05-06 11:41:32 -0400

    profile : allows avatar selection during profil creation
    - sets name and photo to profil.
    - adds avatar selection dialog.
    - allows to take a photo as avatar.
    - allows to choose a picture from disk as avatar.
    - alter photo/picture to 100x100px.
    - allows to save avatar on the computer.
    - handles some case if no webcam can be set by lrc.
    
    Tuleap: #655
    Change-Id: Ib88b66596e83c3e3bf1e38d272ccffbca402a2bc

---
+++ src/avatarmanipulation.h
@@ -0,0 +1,44 @@
+/*
+ *  Copyright (C) 2016 Savoir-faire Linux Inc.
+ *  Author: Nicolas Jager <nicolas.jager@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+#ifndef _AVATARMANIPULATION_H
+#define _AVATARMANIPULATION_H
+
+#include <gtk/gtk.h>
+
+G_BEGIN_DECLS
+
+#define AVATAR_MANIPULATION_TYPE            (avatar_manipulation_get_type ())
+#define AVATAR_MANIPULATION(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), AVATAR_MANIPULATION_TYPE, AvatarManipulation))
+#define AVATAR_MANIPULATION_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST((klass), AVATAR_MANIPULATION_TYPE, AvatarManipulation))
+#define IS_AVATAR_MANIPULATION(obj)         (G_TYPE_CHECK_INSTANCE_TYPE((obj), AVATAR_MANIPULATION_TYPE))
+#define IS_AVATAR_MANIPULATION_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass), AVATAR_MANIPULATION_TYPE))
+
+
+typedef struct _AvatarManipulation      AvatarManipulation;
+typedef struct _AvatarManipulationClass AvatarManipulationClass;
+
+
+GType      avatar_manipulation_get_type (void) G_GNUC_CONST;
+GtkWidget *avatar_manipulation_new      (void);
+void       avatar_manipulation_state_update(AvatarManipulation *self);
+
+G_END_DECLS
+
+#endif /* _AVATARMANIPULATION_H */
