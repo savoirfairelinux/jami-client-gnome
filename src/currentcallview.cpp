4eae4bf0ff5dc9d3e12b18fae96af68a81bf384c
Gasuleg <o.gregoire07@gmail.com>
2016-06-17 18:39:05 -0400

    SmartInfo
    SmartInfo is a tool who will display a lot of information in the client
    (CPU, GPU, Memory usage, Codecs used, percentage of packets lost, bandwidth...).
    These information will be really important for the consumer to understand what happened to the Ring when he don't work properly.
    Daemon: https://gerrit-ring.savoirfairelinux.com/#/c/4315/
    LRC:    https://gerrit-ring.savoirfairelinux.com/#/c/4388/
    Client: https://gerrit-ring.savoirfairelinux.com/#/c/4384/
    
    what this patch can actually do:
    -Menu "Advance information" to launch smartInfo
    -Menu "Stop smartInfo" to stop smartInfo
    
    What I do in this commit:
    Display my first information.
    
    Change-Id: Ia4e19953e69460ee7e006654cba553bfc2d06581

--- src/currentcallview.cpp
+++ src/currentcallview.cpp
@@ -42,6 +42,7 @@
 #include "chatview.h"
 #include <itemdataroles.h>
 #include <numbercategory.h>
+#include <smartInfoHub.h>
 
 static constexpr int CONTROLS_FADE_TIMEOUT = 3000000; /* microseconds */
 static constexpr int FADE_DURATION = 500; /* miliseconds */
@@ -62,11 +63,14 @@ struct _CurrentCallViewPrivate
 {
     GtkWidget *hbox_call_info;
     GtkWidget *hbox_call_controls;
+    GtkWidget *vbox_call_smartInfo;
     GtkWidget *image_peer;
     GtkWidget *label_name;
     GtkWidget *label_uri;
     GtkWidget *label_status;
     GtkWidget *label_duration;
+    GtkWidget *label_smartInfo;
+    GtkWidget *label_smartInfo2;
     GtkWidget *paned_call;
     GtkWidget *frame_video;
     GtkWidget *video_widget;
@@ -100,13 +104,18 @@ struct _CurrentCallViewPrivate
 G_DEFINE_TYPE_WITH_PRIVATE(CurrentCallView, current_call_view, GTK_TYPE_BOX);
 
 #define CURRENT_CALL_VIEW_GET_PRIVATE(obj) (G_TYPE_INSTANCE_GET_PRIVATE ((obj), CURRENT_CALL_VIEW_TYPE, CurrentCallViewPrivate))
-
+//_CurrentCallViewPrivate
 enum {
     VIDEO_DOUBLE_CLICKED,
     LAST_SIGNAL
 };
 
 static guint current_call_view_signals[LAST_SIGNAL] = { 0 };
+
+void
+stop_display_information(){
+    qDebug()<< "into CurrentCallView";
+}
 
 static void
 current_call_view_dispose(GObject *object)
@@ -410,6 +419,7 @@ current_call_view_init(CurrentCallView *view)
     auto stage = gtk_clutter_embed_get_stage(GTK_CLUTTER_EMBED(priv->video_widget));
     auto actor_info = gtk_clutter_actor_new_with_contents(priv->hbox_call_info);
     auto actor_controls = gtk_clutter_actor_new_with_contents(priv->hbox_call_controls);
+    auto actor_smartInfo = gtk_clutter_actor_new_with_contents(priv->vbox_call_smartInfo);
 
     clutter_actor_add_child(stage, actor_info);
     clutter_actor_set_x_align(actor_info, CLUTTER_ACTOR_ALIGN_FILL);
@@ -418,6 +428,10 @@ current_call_view_init(CurrentCallView *view)
     clutter_actor_add_child(stage, actor_controls);
     clutter_actor_set_x_align(actor_controls, CLUTTER_ACTOR_ALIGN_CENTER);
     clutter_actor_set_y_align(actor_controls, CLUTTER_ACTOR_ALIGN_END);
+
+    clutter_actor_add_child(stage, actor_smartInfo);
+    clutter_actor_set_x_align(actor_smartInfo, CLUTTER_ACTOR_ALIGN_CENTER);
+    clutter_actor_set_y_align(actor_smartInfo, CLUTTER_ACTOR_ALIGN_CENTER);
 
     /* add fade in and out states to the info and controls */
     priv->time_last_mouse_motion = g_get_monotonic_time();
@@ -477,11 +491,14 @@ current_call_view_class_init(CurrentCallViewClass *klass)
 
     gtk_widget_class_bind_template_child_private(GTK_WIDGET_CLASS (klass), CurrentCallView, hbox_call_info);
     gtk_widget_class_bind_template_child_private(GTK_WIDGET_CLASS (klass), CurrentCallView, hbox_call_controls);
+    gtk_widget_class_bind_template_child_private(GTK_WIDGET_CLASS (klass), CurrentCallView, vbox_call_smartInfo);
     gtk_widget_class_bind_template_child_private(GTK_WIDGET_CLASS (klass), CurrentCallView, image_peer);
     gtk_widget_class_bind_template_child_private(GTK_WIDGET_CLASS (klass), CurrentCallView, label_name);
     gtk_widget_class_bind_template_child_private(GTK_WIDGET_CLASS (klass), CurrentCallView, label_uri);
     gtk_widget_class_bind_template_child_private(GTK_WIDGET_CLASS (klass), CurrentCallView, label_status);
     gtk_widget_class_bind_template_child_private(GTK_WIDGET_CLASS (klass), CurrentCallView, label_duration);
+    gtk_widget_class_bind_template_child_private(GTK_WIDGET_CLASS (klass), CurrentCallView, label_smartInfo);
+    gtk_widget_class_bind_template_child_private(GTK_WIDGET_CLASS (klass), CurrentCallView, label_smartInfo2);
     gtk_widget_class_bind_template_child_private(GTK_WIDGET_CLASS (klass), CurrentCallView, paned_call);
     gtk_widget_class_bind_template_child_private(GTK_WIDGET_CLASS (klass), CurrentCallView, frame_video);
     gtk_widget_class_bind_template_child_private(GTK_WIDGET_CLASS (klass), CurrentCallView, frame_chat);
@@ -528,6 +545,29 @@ update_details(CurrentCallView *view, Call *call)
     gtk_label_set_text(GTK_LABEL(priv->label_duration), ba_length.constData());
 }
 
+static void
+update_smartInfo(CurrentCallView *view, Call *call)
+{
+    CurrentCallViewPrivate *priv = CURRENT_CALL_VIEW_GET_PRIVATE(view);
+    //gtk_label_set_text(GTK_LABEL(priv->label_smartInfo2), "42");
+
+    QObject::connect(&SmartInfoHub::instance(), &SmartInfoHub::changed,
+        [priv] () {
+
+            gtk_label_set_text(GTK_LABEL(priv->label_smartInfo),SmartInfoHub::instance().localVideoCodec().toStdString().c_str());
+            if(SmartInfoHub::instance().localFps() != 0){
+                gtk_label_set_text(GTK_LABEL(priv->label_smartInfo2),QString::number(SmartInfoHub::instance().localFps()).toStdString().c_str());
+            }
+
+            /*for(int i=0; i < SmartInfoHub::instance().keyChanged().size(); i++){
+                qDebug() << SmartInfoHub::instance().keyChanged().at(i);
+            }
+
+            qDebug() << SmartInfoHub::instance().localFps() LOCAL_FPS;*/
+    });
+}
+
+
 static gboolean
 on_button_press_in_video_event(GtkWidget *self, GdkEventButton *event, CurrentCallView *view)
 {
@@ -572,6 +612,7 @@ current_call_view_set_call_info(CurrentCallView *view, const QModelIndex& idx) {
     /* change some things depending on call state */
     update_state(view, priv->call);
     update_details(view, priv->call);
+    update_smartInfo(view, priv->call);
 
     priv->state_change_connection = QObject::connect(
         priv->call,
@@ -582,7 +623,8 @@ current_call_view_set_call_info(CurrentCallView *view, const QModelIndex& idx) {
     priv->call_details_connection = QObject::connect(
         priv->call,
         static_cast<void (Call::*)(void)>(&Call::changed),
-        [view, priv]() { update_details(view, priv->call); }
+        [view, priv]() { update_details(view, priv->call);
+         update_smartInfo(view, priv->call);}
     );
 
     /* check if we already have a renderer */
