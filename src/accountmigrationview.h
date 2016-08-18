15d91f101967cad01bb526162b84886724f35a1f
aviau <alexandre@alexandreviau.net>
2016-08-18 16:01:09 -0400

    Implement Multi-Device
    This patch implements multi-device support.
    
    - The account creation wizzard has now two options "Existing Ring
      account" and "New Ring account".
    
      "Existing Ring account": Allows for fetching a Ring account archive
      from the DHT. Requires pin and password.
    
      "New Ring account": This is the previously existing wizard. It was
      modified to ask for a password which will be used to encrypt the
      account archive. This password is then required for exporting the
      account on the Ring.
    
    - Creating a new Ring account with the "+" button now triggers the
      account creation wizard.
    
    - The account menu now has a "devices" tab. This tab contains a table
      with device names (currently a short hash) and device ids (a long
      hash).
    
      In the "devices" tab, there is an "add device" button which allows
      for exporting the current account to the Ring, giving a pin to the
      user.
    
    - When the client encounters old-format accounts, it will trigger a
      migration popup which asks the user for a password. This password will
      be used to create an archive and encrypt it. One password will be
      asked for each Ring account to migrate.
    
    Change-Id: I2d579aa270ed1edbfdd7e5f361c6e6078d3fdef1
    Tuleap: #896

---
+++ src/accountmigrationview.h
@@ -0,0 +1,40 @@
+/*
+ *  Copyright (C) 2016 Savoir-faire Linux Inc.
+ *  Author: Alexandre Viau <alexandre.viau@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+#pragma once
+
+#include <gtk/gtk.h>
+
+class Account;
+
+G_BEGIN_DECLS
+
+#define ACCOUNT_MIGRATION_VIEW_TYPE            (account_migration_view_get_type ())
+#define ACCOUNT_MIGRATION_VIEW(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), ACCOUNT_MIGRATION_VIEW_TYPE, AccountMigrationView))
+#define ACCOUNT_MIGRATION_VIEW_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST((klass), ACCOUNT_MIGRATION_VIEW_TYPE, AccountMigrationViewClass))
+#define IS_ACCOUNT_MIGRATION_VIEW(obj)         (G_TYPE_CHECK_INSTANCE_TYPE((obj), ACCOUNT_MIGRATION_VIEW_TYPE))
+#define IS_ACCOUNT_MIGRATION_VIEW_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass), ACCOUNT_MIGRATION_VIEW_TYPE))
+
+typedef struct _AccountMigrationView      AccountMigrationView;
+typedef struct _AccountMigrationViewClass AccountMigrationViewClass;
+
+GType      account_migration_view_get_type      (void) G_GNUC_CONST;
+GtkWidget *account_migration_view_new           (Account *account);
+
+G_END_DECLS
