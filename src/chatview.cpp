f05e38e6e7dbdb98586e35eb5776a59bd320170c
aviau <alexandre@alexandreviau.net>
2016-11-01 12:39:39 -0400

    WebKitChatContainer: Distinguish senders by contactmethods
    Change-Id: Iaab2bfe6914b62e7b96e06e0c806692d155667b9

--- src/chatview.cpp
+++ src/chatview.cpp
@@ -299,16 +299,28 @@ print_text_recording(Media::TextRecording *recording, ChatView *self)
     );
 }
 
-static void
-selected_cm_changed(GtkComboBox *box, ChatView *self)
+ContactMethod*
+get_active_contactmethod(ChatView *self)
 {
-    g_return_if_fail(IS_CHAT_VIEW(self));
     ChatViewPrivate *priv = CHAT_VIEW_GET_PRIVATE(self);
 
     auto cms = priv->person->phoneNumbers();
-    auto active = gtk_combo_box_get_active(box);
+    auto active = gtk_combo_box_get_active(GTK_COMBO_BOX(priv->combobox_cm));
     if (active >= 0 && active < cms.size()) {
-        print_text_recording(cms.at(active)->textRecording(), self);
+        return cms.at(active);
+    } else {
+        return nullptr;
+    }
+}
+
+static void
+selected_cm_changed(G_GNUC_UNUSED GtkComboBox *box, ChatView *self)
+{
+    g_return_if_fail(IS_CHAT_VIEW(self));
+
+    auto cm = get_active_contactmethod(self);
+    if (cm){
+        print_text_recording(cm->textRecording(), self);
     } else {
         g_warning("no valid ContactMethod selected to display chat conversation");
     }
@@ -422,6 +434,42 @@ set_participant_images(ChatView* self)
 {
     ChatViewPrivate *priv = CHAT_VIEW_GET_PRIVATE(self);
 
+    webkit_chat_container_clear_sender_images(
+        WEBKIT_CHAT_CONTAINER(priv->webkit_chat_container)
+    );
+
+    /* Set the sender image for the peer */
+    ContactMethod* sender_contact_method_peer;
+    QVariant photo_variant_peer;
+
+    if (priv->person)
+    {
+        photo_variant_peer = priv->person->photo();
+        sender_contact_method_peer = get_active_contactmethod(self);
+    }
+    else
+    {
+        if (priv->cm)
+        {
+            sender_contact_method_peer = priv->cm;
+        }
+        else
+        {
+            sender_contact_method_peer = priv->call->peerContactMethod();
+        }
+        photo_variant_peer = sender_contact_method_peer->roleData((int) Call::Role::Photo);
+    }
+
+    if (photo_variant_peer.isValid())
+    {
+        g_warning("SET SENDER IMAGE FOR PEER");
+        webkit_chat_container_set_sender_image(
+            WEBKIT_CHAT_CONTAINER(priv->webkit_chat_container),
+            sender_contact_method_peer,
+            photo_variant_peer
+        );
+    }
+
     /* set sender image for "ME" */
     auto profile = ProfileModel::instance().selectedProfile();
     if (profile)
@@ -434,44 +482,11 @@ set_participant_images(ChatView* self)
             {
                 webkit_chat_container_set_sender_image(
                     WEBKIT_CHAT_CONTAINER(priv->webkit_chat_container),
-                    "Me",
+                    nullptr,
                     photo_variant_me
                 );
             }
         }
-    }
-
-    /* Set the sender image for the peer */
-    QString sender_name_peer;
-    QVariant photo_variant_peer;
-
-    if (priv->person)
-    {
-        photo_variant_peer = priv->person->photo();
-        sender_name_peer = priv->person->roleData(static_cast<int>(Ring::Role::Name)).toString();
-    }
-    else
-    {
-        ContactMethod *contact_method;
-        if (priv->cm)
-        {
-            contact_method = priv->cm;
-        }
-        else
-        {
-            contact_method = priv->call->peerContactMethod();
-        }
-        sender_name_peer = contact_method->roleData(static_cast<int>(Ring::Role::Name)).toString();
-        photo_variant_peer = contact_method->roleData((int) Call::Role::Photo);
-    }
-
-    if (photo_variant_peer.isValid())
-    {
-        webkit_chat_container_set_sender_image(
-            WEBKIT_CHAT_CONTAINER(priv->webkit_chat_container),
-            sender_name_peer,
-            photo_variant_peer
-        );
     }
 }
 
