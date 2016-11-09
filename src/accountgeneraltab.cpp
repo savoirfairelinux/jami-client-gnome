b6162104e55cba7f942ced49078ed91f5a922cf4
aviau <alexandre@alexandreviau.net>
2016-11-09 16:41:53 -0500

    New account creation wizard standardization
    Adapt to the terminilogy that was decided for all clients:
     - displayName: Full Name
     - blockchain username: Username
    
    Changed buttons order:
     1. Create Account
     2. Link account
    
    Add question mark next to register username checkbox so that the tooltip
    is easier to find.
    
    Add question mark and tooltip next to "choose password" label.
    
    Split first page of account creation wizard into two distinct
    categories: Ring Account and Profile.
    
    Tuleap: #1296
    Change-Id: Ia70e638ccd2462f6123800c71a2fea07ca0552df

--- src/accountgeneraltab.cpp
+++ src/accountgeneraltab.cpp
@@ -316,7 +316,7 @@ build_tab_view(AccountGeneralTab *view)
         gtk_grid_attach(GTK_GRID(priv->grid_account), entry_username, 1, grid_row, 1, 1);
         ++grid_row;
 
-        label = gtk_label_new("Ring username");
+        label = gtk_label_new(_("Username"));
         gtk_widget_set_halign(label, GTK_ALIGN_START);
         gtk_grid_attach(GTK_GRID(priv->grid_account), label, 0, grid_row, 1, 1);
         auto username_registration_box = username_registration_box_new(priv->account, TRUE);
