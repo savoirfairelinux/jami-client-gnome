f5b353c9772b176514af077c456de3349bcd0cfb
Nicolas Jager <nicolas.jager@savoirfairelinux.com>
2017-04-28 12:16:43 -0400

    pour envoyer et afficher un message avec trust request
    - je crois qu'il y a une regression avec l'affichage de l'avatar
    dans la page de contact request. je regarderai ca lundi matin.
    
    Change-Id: Ie646701bcc05f155be70f3b4d1146e940432fd1b

--- src/contactrequestcontentview.cpp
+++ src/contactrequestcontentview.cpp
@@ -77,6 +77,7 @@ struct _ContactRequestContentViewPrivate
     GtkWidget*    button_block_contact_request;
     GtkWidget*    image_peer;
     GtkWidget*    label_bestId;
+    GtkWidget*    text_view_message;
 };
 
 G_DEFINE_TYPE_WITH_PRIVATE(ContactRequestContentView, contact_request_content_view, GTK_TYPE_BOX);
@@ -166,6 +167,7 @@ contact_request_content_view_class_init(ContactRequestContentViewClass *klass)
     gtk_widget_class_bind_template_child_private(GTK_WIDGET_CLASS (klass), ContactRequestContentView, button_close_contact_request_content_view);
     gtk_widget_class_bind_template_child_private(GTK_WIDGET_CLASS (klass), ContactRequestContentView, image_peer);
     gtk_widget_class_bind_template_child_private(GTK_WIDGET_CLASS (klass), ContactRequestContentView, label_bestId);
+    gtk_widget_class_bind_template_child_private(GTK_WIDGET_CLASS (klass), ContactRequestContentView, text_view_message);
 
     contact_request_content_view_signals[HIDE_VIEW_CLICKED] = g_signal_new (
         "hide-view-clicked",
@@ -204,6 +206,15 @@ build_contact_request_content(ContactRequestContentView *self, ContactRequest *c
         gtk_label_set_text(GTK_LABEL(priv->label_bestId), contactId_std.c_str());
         gtk_widget_show(priv->label_bestId);
     }
+
+    gtk_label_set_text(GTK_LABEL(priv->label_peer), contact_request->certificate()->remoteId().constData());
+
+    /* show the message */
+    auto message = contact_request->message().toStdString();
+    GtkTextBuffer* buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(priv->text_view_message));
+    gtk_text_buffer_insert_at_cursor (buffer, message.c_str(), message.size());
+    gtk_text_view_set_buffer(GTK_TEXT_VIEW(priv->text_view_message), buffer);
+
 }
 
 /**
