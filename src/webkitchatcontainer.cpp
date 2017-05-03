9d8c7bb08b5fa20134b76d2a1e564ce71957b700
Stepan Salenikovich <stepan.salenikovich@savoirfairelinux.com>
2017-05-03 16:12:11 -0400

    re-create WebView when process crashes
    Occasionally the WebKit2Gtk web process crashes which results in
    the chat not being visible until the user restarts the program.
    Instead, we can detect this and re-create a new WebView when this
    happens.
    
    Change-Id: Id84ad9cbbd85082dfff5856828a3e9eeb32abf10

--- src/webkitchatcontainer.cpp
+++ src/webkitchatcontainer.cpp
@@ -73,9 +73,14 @@ enum {
 
 static guint webkit_chat_container_signals[LAST_SIGNAL] = { 0 };
 
+/* functions */
+static gboolean webview_crashed(WebKitChatContainer *self);
+
 static void
 webkit_chat_container_dispose(GObject *object)
 {
+    g_warning("disposing of webkit chat container");
+
     G_OBJECT_CLASS(webkit_chat_container_parent_class)->dispose(object);
 }
 
@@ -335,7 +340,6 @@ build_view(WebKitChatContainer *view)
     g_return_if_fail(IS_WEBKIT_CHAT_CONTAINER(view));
     WebKitChatContainerPrivate *priv = WEBKIT_CHAT_CONTAINER_GET_PRIVATE(view);
 
-
     priv->chatview_debug = FALSE;
     auto ring_chatview_debug = g_getenv("RING_CHATVIEW_DEBUG");
     if (ring_chatview_debug || g_strcmp0(ring_chatview_debug, "true") == 0)
@@ -408,6 +412,27 @@ build_view(WebKitChatContainer *view)
 
     /* Now we wait for the load-changed event, before we
      * start loading javascript libraries */
+
+    /* handle web view crash */
+    g_signal_connect_swapped(priv->webview_chat, "web-process-crashed", G_CALLBACK(webview_crashed), view);
+}
+
+static gboolean
+webview_crashed(WebKitChatContainer *self)
+{
+    g_warning("Gtk Web Process crashed! Re-createing web view");
+
+    auto priv = WEBKIT_CHAT_CONTAINER_GET_PRIVATE(self);
+
+    /* make sure we destroy previous WebView */
+    if (priv->webview_chat) {
+        gtk_container_remove(GTK_CONTAINER(priv->box_webview_chat), priv->webview_chat);
+        priv->webview_chat = nullptr;
+    }
+
+    build_view(self);
+
+    return G_SOURCE_CONTINUE;
 }
 
 GtkWidget *
