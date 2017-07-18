0c43e51c42a5b6dda6d7e33a8d0034770dfaaa89
Anthony Léonard <anthony.leonard@savoirfairelinux.com>
2017-07-18 09:40:39 -0400

    update version number and name
    “Beta 2” mention has been removed, replaced by “Version 1.0”. The name
    of the version is no longer ”Gaston Miron” but “Liberté, Égalité,
    Fraternité”.
    
    Change-Id: I224db4cf18aec0dd0f4435389955855260db7232

--- src/dialogs.c
+++ src/dialogs.c
@@ -81,7 +81,7 @@ ring_about_dialog(GtkWidget *parent)
     }
 
     gchar *version = g_strdup_printf(C_("Do not translate the release name nor the status (beta, final, ...)",
-                                        "Gaston Miron - Beta 2\nbuilt on %.25s"),
+                                        "Liberté, Égalité, Fraternité - Version 1.0\nbuilt on %.25s"),
                                      RING_CLIENT_BUILD_DATE);
 
     const gchar *authors[] = {
