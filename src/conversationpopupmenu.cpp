feb07406523ea082ca36152098f24b966f93a777
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2017-08-23 16:03:59 -0400

    WIP show contacts from conversationmodel
    Change-Id: Ic4f68bc887b7c2265294b88f36a54efa2bf28dc7

---
+++ src/conversationpopupmenu.cpp
@@ -0,0 +1,198 @@
+/****************************************************************************
+ *   Copyright (C) 2017 Savoir-faire Linux                                  *
+ *   Author : Nicolas Jäger <nicolas.jager@savoirfairelinux.com>            *
+ *   Author : Sébastien Blin <sebastien.blin@savoirfairelinux.com>          *
+ *                                                                          *
+ *   This library is free software; you can redistribute it and/or          *
+ *   modify it under the terms of the GNU Lesser General Public             *
+ *   License as published by the Free Software Foundation; either           *
+ *   version 2.1 of the License, or (at your option) any later version.     *
+ *                                                                          *
+ *   This library is distributed in the hope that it will be useful,        *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      *
+ *   Lesser General Public License for more details.                        *
+ *                                                                          *
+ *   You should have received a copy of the GNU General Public License      *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+
+#include "conversationpopupmenu.h"
+
+// GTK+ related
+#include <glib/gi18n.h>
+#include <QString>
+
+// LRC
+
+struct _ConversationPopupMenu
+{
+    GtkMenu parent;
+};
+
+struct _ConversationPopupMenuClass
+{
+    GtkMenuClass parent_class;
+};
+
+typedef struct _ConversationPopupMenuPrivate ConversationPopupMenuPrivate;
+
+struct _ConversationPopupMenuPrivate
+{
+    GtkTreeView *treeview;
+
+    std::shared_ptr<ConversationModel> conversationModel_;
+};
+
+G_DEFINE_TYPE_WITH_PRIVATE(ConversationPopupMenu, conversation_popup_menu, GTK_TYPE_MENU);
+
+#define CONVERSATION_POPUP_MENU_GET_PRIVATE(obj) (G_TYPE_INSTANCE_GET_PRIVATE ((obj), CONVERSATION_POPUP_MENU_TYPE, ConversationPopupMenuPrivate))
+
+static void
+remove_history_conversation(GtkWidget *menu, gint* row)
+{
+    ConversationPopupMenuPrivate *priv = CONVERSATION_POPUP_MENU_GET_PRIVATE(menu);
+    try
+    {
+        auto conversation = priv->conversationModel_->getConversation(*row);
+        priv->conversationModel_->cleanHistory(conversation.uid_);
+    }
+    catch (const std::exception&)
+    {
+        g_warning("Can't get conversation at row %i", *row);
+    }
+}
+
+static void
+remove_conversation(G_GNUC_UNUSED GtkWidget *menu, gint* row)
+{
+    ConversationPopupMenuPrivate *priv = CONVERSATION_POPUP_MENU_GET_PRIVATE(menu);
+    try
+    {
+        auto conversation = priv->conversationModel_->getConversation(*row);
+        priv->conversationModel_->removeConversation(conversation.uid_);
+    }
+    catch (const std::exception&)
+    {
+        g_warning("Can't get conversation at row %i", *row);
+    }
+}
+
+static void
+add_conversation(G_GNUC_UNUSED GtkWidget *menu, gint* row)
+{
+    //TODO
+}
+
+static void
+place_call(G_GNUC_UNUSED GtkWidget *menu, gint* row)
+{
+    ConversationPopupMenuPrivate *priv = CONVERSATION_POPUP_MENU_GET_PRIVATE(menu);
+    try
+    {
+        auto conversation = priv->conversationModel_->getConversation(*row);
+        priv->conversationModel_->placeCall(conversation.uid_);
+    }
+    catch (const std::exception&)
+    {
+        g_warning("Can't get conversation at row %i", *row);
+    }
+}
+
+/**
+ * Update the menu when the selected conversation in the treeview changes.
+ */
+static void
+update(GtkTreeSelection *selection, ConversationPopupMenu *self)
+{
+    ConversationPopupMenuPrivate *priv = CONVERSATION_POPUP_MENU_GET_PRIVATE(self);
+    /* clear the current menu */
+    gtk_container_forall(GTK_CONTAINER(self), (GtkCallback)gtk_widget_destroy, nullptr);
+
+    // Retrieve conversation
+    GtkTreeIter iter;
+    GtkTreeModel *model;
+    if (!gtk_tree_selection_get_selected(selection, &model, &iter))
+        return;
+    auto path = gtk_tree_model_get_path(model, &iter);
+    auto idx = gtk_tree_path_get_indices(path);
+    auto conversation = priv->conversationModel_->getConversation(idx[0]);
+
+    /* we always build a menu, however in some cases some or all of the conversations will be deactivated;
+     * we prefer this to having an empty menu because GTK+ behaves weird in the empty menu case
+     */
+    auto place_call_conversation = gtk_menu_item_new_with_mnemonic(_("_Place call"));
+    gtk_menu_shell_append(GTK_MENU_SHELL(self), place_call_conversation);
+    g_signal_connect(place_call_conversation, "activate", G_CALLBACK(place_call), idx);
+    if (conversation.isUsed_) {
+        // If we can add this conversation
+        auto add_conversation_conversation = gtk_menu_item_new_with_mnemonic(_("_Add conversation"));
+        gtk_menu_shell_append(GTK_MENU_SHELL(self), add_conversation_conversation);
+        g_signal_connect(add_conversation_conversation, "activate", G_CALLBACK(add_conversation), idx);
+    } else {
+        auto rm_history_conversation = gtk_menu_item_new_with_mnemonic(_("_Clear history"));
+        gtk_menu_shell_append(GTK_MENU_SHELL(self), rm_history_conversation);
+        g_signal_connect(rm_history_conversation, "activate", G_CALLBACK(remove_history_conversation), idx);
+        auto rm_conversation_conversation = gtk_menu_item_new_with_mnemonic(_("_Remove conversation"));
+        gtk_menu_shell_append(GTK_MENU_SHELL(self), rm_conversation_conversation);
+        g_signal_connect(rm_conversation_conversation, "activate", G_CALLBACK(remove_conversation), idx);
+    }
+
+    /* show all conversations */
+    gtk_widget_show_all(GTK_WIDGET(self));
+}
+
+static void
+conversation_popup_menu_dispose(GObject *object)
+{
+    G_OBJECT_CLASS(conversation_popup_menu_parent_class)->dispose(object);
+}
+
+static void
+conversation_popup_menu_finalize(GObject *object)
+{
+    G_OBJECT_CLASS(conversation_popup_menu_parent_class)->finalize(object);
+}
+
+static void
+conversation_popup_menu_class_init(ConversationPopupMenuClass *klass)
+{
+    G_OBJECT_CLASS(klass)->finalize = conversation_popup_menu_finalize;
+    G_OBJECT_CLASS(klass)->dispose = conversation_popup_menu_dispose;
+}
+
+
+static void
+conversation_popup_menu_init(G_GNUC_UNUSED ConversationPopupMenu *self)
+{
+    // nothing to do
+}
+
+GtkWidget *
+conversation_popup_menu_new(GtkTreeView *treeview, std::shared_ptr<ConversationModel> conversationModel)
+{
+    gpointer self = g_object_new(CONVERSATION_POPUP_MENU_TYPE, NULL);
+    ConversationPopupMenuPrivate *priv = CONVERSATION_POPUP_MENU_GET_PRIVATE(self);
+    priv->conversationModel_ = conversationModel;
+
+    priv->treeview = treeview;
+    GtkTreeSelection *selection = gtk_tree_view_get_selection(priv->treeview);
+    g_signal_connect(selection, "changed", G_CALLBACK(update), self);
+
+    // build the menu for the first time
+    update(selection, CONVERSATION_POPUP_MENU(self));
+
+    return (GtkWidget *)self;
+}
+
+gboolean
+conversation_popup_menu_show(ConversationPopupMenu *self, GdkEventButton *event)
+{
+    /* check for right click */
+    if (event->type == GDK_BUTTON_PRESS && event->button == GDK_BUTTON_SECONDARY ) {
+        /* the menu will automatically get updated when the selection changes */
+        gtk_menu_popup(GTK_MENU(self), NULL, NULL, NULL, NULL, event->button, event->time);
+    }
+
+    return GDK_EVENT_PROPAGATE; /* so that the conversation selection changes */
+}
