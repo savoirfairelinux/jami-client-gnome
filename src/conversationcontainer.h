bfca94fcfa4f0b60816e44406f604330b49482eb
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2017-08-28 17:36:16 -0400

    update gnome client
    Change-Id: Iaa2c93e0224c08daf8e1476d61b87a8071fc0b6b

---
+++ src/conversationcontainer.h
@@ -0,0 +1,14 @@
+#pragma once
+
+// Lrc
+#include <api/conversation.h>
+
+class ConversationContainer {
+
+public:
+    explicit ConversationContainer (const lrc::api::conversation::Info& cInfo)
+    : info(cInfo)
+    {}
+
+    const lrc::api::conversation::Info& info;
+};
