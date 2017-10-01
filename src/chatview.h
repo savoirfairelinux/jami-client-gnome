583a936b3373abf7044cba08d2c53196321bc314
Sébastien Blin <contact@enconn.fr>
2017-10-01 16:19:02 -0400

    improve conversations display
    Change-Id: I9c1b9edfb8f5184b07e3ddaeb22530fd220d3831

--- src/chatview.h
+++ src/chatview.h
@@ -47,6 +47,6 @@ GtkWidget     *chat_view_new        (WebKitChatContainer* view,
 lrc::api::conversation::Info chat_view_get_conversation(ChatView*);
 bool chat_view_get_temporary(ChatView*);
 void chat_view_update_temporary(ChatView*, bool);
-void           chat_view_set_header_visible(ChatView*, gboolean);
+void chat_view_set_header_visible(ChatView*, gboolean);
 
 G_END_DECLS
