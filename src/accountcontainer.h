1e92ef0dbe2e74f31ee47ccfc0d1eb7caa34f1f3
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2017-10-03 11:25:02 -0400

    conversationpopupmenu: add popup menu for conversations
    This class will replace contactpopupmenu to use the new lrc.
    
    Change-Id: I4fefd530484288f03a0fd0df7d1ccf7df6f71ea9

---
+++ src/accountcontainer.h
@@ -0,0 +1,37 @@
+/****************************************************************************
+ *   Copyright (C) 2017 Savoir-faire Linux                                  *
+ *   Author: Nicolas Jäger <nicolas.jager@savoirfairelinux.com>             *
+ *   Author: Sébastien Blin <sebastien.blin@savoirfairelinux.com>           *
+ *                                                                          *
+ *   This library is free software; you can redistribute it and/or          *
+ *   modify it under the terms of the GNU Lesser General Public             *
+ *   License as published by the Free Software Foundation; either           *
+ *   version 2.1 of the License, or (at your option) any later version.     *
+ *                                                                          *
+ *   This library is distributed in the hope that it will be useful,        *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      *
+ *   Lesser General Public License for more details.                        *
+ *                                                                          *
+ *   You should have received a copy of the GNU General Public License      *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+#pragma once
+
+// Lrc
+#include <api/account.h>
+
+/**
+ * This class contains a const reference linked to an account.
+ * NOTE: it avoids weird initialization with Gtk
+ * @param  accInfo the account linked
+ */
+class AccountContainer {
+
+public:
+    explicit AccountContainer (const lrc::api::account::Info& accInfo)
+    : info(accInfo)
+    {}
+
+    const lrc::api::account::Info& info;
+};
