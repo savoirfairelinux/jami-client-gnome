6266d017272035f460dd334c803046b47b3fbfa4
Guillaume Roguez <guillaume.roguez@savoirfairelinux.com>
2018-01-30 15:54:02 -0500

    copyright date update
    Change-Id: Ifb26147e6faf37f5ac4f5ffac582ee069902d382

--- src/bannedcontactsview.cpp
+++ src/bannedcontactsview.cpp
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2017 Savoir-faire Linux Inc.
+ *  Copyright (C) 2017-2018 Savoir-faire Linux Inc.
  *  Author: Nicolas Jäger <nicolas.jager@savoirfairelinux.com>
  *
  *  This program is free software; you can redistribute it and/or modify
