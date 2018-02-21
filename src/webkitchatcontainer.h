05317a74f1b0dcb3babd274b1c4e980632f6bf2a
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2018-02-21 11:09:16 -0500

    chatview: improve large history loading
    When rendering large history, the webkitprocess is using a lot of
    ressources and freeze during a few seconds.
    
    This patch allows the user to directly have the ability to see
    previous history and send messages while history is rendering.
    
    Change-Id: I921070439328f7bfecb57bfbaec43c3dd88d2d14
    Reviewed-by: Philippe Gorley <philippe.gorley@savoirfairelinux.com>

--- src/webkitchatcontainer.h
+++ src/webkitchatcontainer.h
@@ -22,6 +22,7 @@
 
 // Gtk
 #include <gtk/gtk.h>
+#include <map>
 
 // LRC
 #include <api/interaction.h>
@@ -42,6 +43,7 @@ GtkWidget* webkit_chat_container_new                  (void);
 void       webkit_chat_container_clear                (WebKitChatContainer *view);
 void       webkit_chat_container_clear_sender_images  (WebKitChatContainer *view);
 void       webkit_chat_container_print_new_interaction(WebKitChatContainer *view, uint64_t msgId, const lrc::api::interaction::Info& interaction);
+void       webkit_chat_container_print_history        (WebKitChatContainer *view, const std::map<uint64_t, lrc::api::interaction::Info> interactions);
 void       webkit_chat_container_update_interaction   (WebKitChatContainer *view, uint64_t msgId, const lrc::api::interaction::Info& interaction);
 void       webkit_chat_container_set_sender_image     (WebKitChatContainer *view, const std::string& sender, const std::string& senderImage);
 gboolean   webkit_chat_container_is_ready             (WebKitChatContainer *view);
