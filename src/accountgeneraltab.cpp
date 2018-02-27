6f04c788856fc8a80bed835b5846854cfe6a8b36
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2018-02-27 16:46:20 -0500

    accountgeneraltab: add the ability to export an account to a file
    Change-Id: Idd456a7b1ef7d5ed5d5128157650f04b7c94eaf9

--- src/accountgeneraltab.cpp
+++ src/accountgeneraltab.cpp
@@ -50,6 +50,9 @@ struct _AccountGeneralTabPrivate
     GtkWidget* button_validate_password;
     GtkWidget* label_error_change_passowrd;
 
+    GtkWidget* button_choose_file;
+    GtkWidget* label_export_informations;
+
     QMetaObject::Connection account_updated;
 };
 
@@ -91,6 +94,10 @@ account_general_tab_class_init(AccountGeneralTabClass *klass)
     gtk_widget_class_bind_template_child_private(GTK_WIDGET_CLASS (klass), AccountGeneralTab, entry_confirm_password);
     gtk_widget_class_bind_template_child_private(GTK_WIDGET_CLASS (klass), AccountGeneralTab, button_validate_password);
     gtk_widget_class_bind_template_child_private(GTK_WIDGET_CLASS (klass), AccountGeneralTab, label_error_change_passowrd);
+
+    /* export account */
+    gtk_widget_class_bind_template_child_private(GTK_WIDGET_CLASS (klass), AccountGeneralTab, button_choose_file);
+    gtk_widget_class_bind_template_child_private(GTK_WIDGET_CLASS (klass), AccountGeneralTab, label_export_informations);
 
 }
 
@@ -181,6 +188,44 @@ reset_change_password(AccountGeneralTab *view)
     AccountGeneralTabPrivate *priv = ACCOUNT_GENERAL_TAB_GET_PRIVATE(view);
     gtk_label_set_text(GTK_LABEL(priv->label_error_change_passowrd), "");
     gtk_button_set_label(GTK_BUTTON(priv->button_validate_password), _("Change password"));
+}
+
+
+static void
+choose_export_file(AccountGeneralTab *view)
+{
+    g_return_if_fail(IS_ACCOUNT_GENERAL_TAB(view));
+    AccountGeneralTabPrivate *priv = ACCOUNT_GENERAL_TAB_GET_PRIVATE(view);
+    // Sauvegarder path
+    GtkWidget* dialog;
+    GtkFileChooserAction action = GTK_FILE_CHOOSER_ACTION_SAVE;
+    gint res;
+    gchar* filename = nullptr;
+
+    dialog = gtk_file_chooser_dialog_new(_("Save File"),
+                                         GTK_WINDOW(gtk_widget_get_toplevel(GTK_WIDGET(view))),
+                                         action,
+                                         _("_Cancel"),
+                                         GTK_RESPONSE_CANCEL,
+                                         _("_Save"),
+                                         GTK_RESPONSE_ACCEPT,
+                                         nullptr);
+
+    res = gtk_dialog_run (GTK_DIALOG(dialog));
+
+    if (res == GTK_RESPONSE_ACCEPT) {
+        auto chooser = GTK_FILE_CHOOSER(dialog);
+        filename = gtk_file_chooser_get_filename(chooser);
+    }
+    gtk_widget_destroy (dialog);
+
+    if (!filename) return;
+
+    // export account
+    priv->account->exportToFile(filename);
+    // Set informations label
+    std::string label = std::string(_("File exported to destination: ")) + std::string(filename);
+    gtk_label_set_text(GTK_LABEL(priv->label_export_informations), label.c_str());
 }
 
 static void
@@ -412,6 +457,7 @@ build_tab_view(AccountGeneralTab *view)
     g_signal_connect_swapped(priv->entry_current_password, "changed", G_CALLBACK(reset_change_password), view);
     g_signal_connect_swapped(priv->entry_new_password, "changed", G_CALLBACK(reset_change_password), view);
     g_signal_connect_swapped(priv->entry_confirm_password, "changed", G_CALLBACK(reset_change_password), view);
+    g_signal_connect_swapped(priv->button_choose_file, "clicked", G_CALLBACK(choose_export_file), view);
     gtk_widget_set_visible(priv->entry_current_password, priv->account->archiveHasPassword());
 
     gtk_widget_show_all(priv->grid_parameters);
