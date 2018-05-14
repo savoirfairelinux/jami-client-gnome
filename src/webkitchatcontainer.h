edad883223091c2cc81978c260837e9eaf0658b8
Hugo Lefeuvre <hugo.lefeuvre@savoirfairelinux.com>
2018-05-14 16:36:06 -0400

    chatview: implement navbar
    Currently the navbar is implemented in native GTK, and has a pretty
    bad integration with the chatview. In this patch we implement a
    modern navbar *in* the chatview and remove the old GTK bar.
    
    *Changes summary*
    
    chatview, HTML/JS/CSS side:
    - Remove useless function setSendIcon from the chatview
    - Simplify setTemporary in the chatview
    - Use only one single showInvitation function instead of two
      showInvitation and hideInvitation functions.
    - Remove pointless/copy&pasted comments
    
    chatview, GTK side:
    - Simplify chatview update methods: instead of providing several
    separate functions to update the invitation/banned status of the chat
    view, only provide one (update_chatview_frame). This should have a
    good impact on the performances and simplify webkit crash handling.
    
    Change-Id: I6959240efd357fedb07d3c60d551efc8fca84812
    Reviewed-by: Sebastien Blin <sebastien.blin@savoirfairelinux.com>

--- src/webkitchatcontainer.h
+++ src/webkitchatcontainer.h
@@ -54,7 +54,8 @@ gboolean   webkit_chat_container_is_ready             (WebKitChatContainer *view
 void       webkit_chat_container_set_display_links    (WebKitChatContainer *view, bool display);
 void       webkit_chat_disable_send_interaction       (WebKitChatContainer *view, bool isDisabled);
 void       webkit_chat_hide_message_bar               (WebKitChatContainer *view, bool isHidden);
-void       webkit_chat_container_set_temporary        (WebKitChatContainer *view, bool temporary);
 void       webkit_chat_container_set_invitation       (WebKitChatContainer *view, bool show, const std::string& contactUri);
+void       webkit_chat_set_header_visible             (WebKitChatContainer *view, bool isVisible);
+void       webkit_chat_update_chatview_frame          (WebKitChatContainer *view, bool isBanned, bool isInvited, const gchar* alias, const gchar* bestId);
 
 G_END_DECLS
