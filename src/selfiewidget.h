9b2d86f648823d7db47e8708f563af345c1b2abb
Hugo Lefeuvre <hugo.lefeuvre@savoirfairelinux.com>
2018-06-05 12:14:00 -0400

    selfieview: initial import
    First import of the selfie view.
    
    TODO: Better description.
    
    Change-Id: If64bf917f69ac56fde4c3cf69456f91774c763cd

---
+++ src/selfiewidget.h
@@ -0,0 +1,42 @@
+/*
+ *  Copyright (C) 2018 Savoir-faire Linux Inc.
+ *  Author: Hugo Lefeuvre <hugo.lefeuvre@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+#ifndef _SELFIEWIDGET_H
+#define _SELFIEWIDGET_H
+
+#include <gtk/gtk.h>
+
+G_BEGIN_DECLS
+
+#define SELFIE_WIDGET_TYPE            (selfie_widget_get_type ())
+#define SELFIE_WIDGET(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), SELFIE_WIDGET_TYPE, SelfieWidget))
+#define SELFIE_WIDGET_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST((klass), SELFIE_WIDGET_TYPE, SelfieWidget))
+#define IS_SELFIE_WIDGET(obj)         (G_TYPE_CHECK_INSTANCE_TYPE((obj), SELFIE_WIDGET_TYPE))
+#define IS_SELFIE_WIDGET_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass), SELFIE_WIDGET_TYPE))
+
+typedef struct _SelfieWidget      SelfieWidget;
+typedef struct _SelfieWidgetClass SelfieWidgetClass;
+
+GType       selfie_widget_get_type      (void) G_GNUC_CONST;
+GtkWidget   *selfie_widget_new          (void);
+const char  *selfie_widget_get_filename (SelfieWidget *self);
+
+G_END_DECLS
+
+#endif /* _SELFIEWIDGET_H */
