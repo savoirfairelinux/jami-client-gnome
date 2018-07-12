01efe35f6236ac9fdd3d9c0ad5eff405e9981542
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2018-07-12 15:28:38 -0400

    ringnotify: rewrite notification system
    Ring will now use three types of notifications:
    
    1. Call notifications: opened when an incoming call is here
    and closed at the end of the call.
    2. Request notifications: opened when a new trust request
    arrives and closed when the user accepts/refuse/block or
    just open the conversation.
    3. Chat notifications: arrives with new interactions and if
    the current conversation is different or the client not focused. Closed
    by Gnome or when the conversation is opened.
    
    Change-Id: I5e5abf20507bac8bb37c429bc929c671fe66bd6b
    Gitlab: #868

--- src/ringnotify.h
+++ src/ringnotify.h
@@ -1,6 +1,7 @@
 /*
  *  Copyright (C) 2015-2018 Savoir-faire Linux Inc.
  *  Author: Stepan Salenikovich <stepan.salenikovich@savoirfairelinux.com>
+ *  Author: Sebastien Blin <sebastien.blin@savoirfairelinux.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -21,22 +22,35 @@
 #define RING_NOTIFY_H_
 
 #include <gtk/gtk.h>
-#include "ringmainwindow.h"
-
-class Call;
-class ContactMethod;
-namespace Media {
-class TextRecording;
-}
+#include <string>
 
 G_BEGIN_DECLS
+#define RING_NOTIFIER_TYPE            (ring_notifier_get_type ())
+#define RING_NOTIFIER(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), RING_NOTIFIER_TYPE, RingNotifier))
+#define RING_NOTIFIER_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST((klass), RING_NOTIFIER_TYPE, RingNotifierClass))
+#define IS_RING_NOTIFIER(obj)         (G_TYPE_CHECK_INSTANCE_TYPE((obj), RING_NOTIFIER_TYPE))
+#define IS_RING_NOTIFIER_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass), RING_NOTIFIER_TYPE))
 
-void     ring_notify_init();
-void     ring_notify_uninit();
-gboolean ring_notify_is_initted();
-gboolean ring_notify_incoming_call(Call*);
-void     ring_notify_message(ContactMethod*, Media::TextRecording*);
-gboolean ring_notify_close_chat_notification(ContactMethod*);
+typedef struct _RingNotifier      RingNotifier;
+typedef struct _RingNotifierClass RingNotifierClass;
+
+enum class NotificationType
+{
+    CALL,
+    REQUEST,
+    CHAT
+};
+
+GType      ring_notifier_get_type             (void) G_GNUC_CONST;
+GtkWidget* ring_notifier_new                  (void);
+
+gboolean    ring_show_notification(RingNotifier* view,
+                                   const std::string& icon,
+                                   const std::string& id,
+                                   const std::string& title,
+                                   const std::string& body,
+                                   NotificationType type);
+gboolean    ring_hide_notification(RingNotifier* view, const std::string& id);
 
 G_END_DECLS
 
