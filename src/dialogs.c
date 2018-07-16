8d7b309a743e9c7a6233e50169f3635ad3cd06e0
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2018-07-16 16:34:59 -0400

    dialogs: bump release name to Live Free or Die
    Change-Id: I194f452a69fa7231adbe2cbf7c25fc3180206cf9
    Reviewed-by: Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>

--- src/dialogs.c
+++ src/dialogs.c
@@ -81,7 +81,7 @@ ring_about_dialog(GtkWidget *parent)
     }
 
     gchar *version = g_strdup_printf(C_("Do not translate the release name nor the status (beta, final, ...)",
-                                        "\"Live free or die\"\nbuilt on %.25s"),
+                                        "\"Live Free or Die\"\nbuilt on %.25s"),
                                      RING_CLIENT_BUILD_DATE);
 
     const gchar *authors[] = {
