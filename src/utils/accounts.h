643a7469fef62ecc5604aef8dca86ba45e7a0ba2
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2018-07-24 10:11:28 -0400

    misc: remove unused code from old LRC models
    Re-add nm connectivity signals
    
    Change-Id: I4677db795cf43326ffecd44e3b561c98c337ffb4
    Gitlab: #984

---
+++ src/utils/accounts.h
@@ -0,0 +1,35 @@
+/*
+ *  Copyright (C) 2015-2018 Savoir-faire Linux Inc.
+ *  Author: Stepan Salenikovich <stepan.salenikovich@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+#ifndef _ACCOUNTS_H
+#define _ACCOUNTS_H
+
+#include <gtk/gtk.h>
+
+class Account;
+
+/**
+ * iterates through all existing accounts and make sure all RING accounts have
+ * a display name set; if a display name is empty, it is set to the alias of the
+ * account
+ */
+void
+force_ring_display_name();
+
+#endif /* _ACCOUNTS_H */
