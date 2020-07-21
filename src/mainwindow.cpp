948a940049816b144a5a5e63ee0486a186fc8f20
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2020-07-21 11:34:01 -0400

    conferences: add ability to change layouts
    The UI/UX is not perfect as we can't click on the renderer for now,
    but this will change in the future
    
    Change-Id: I0e92abbdd3e0f28ae161eea5e663cc21da58a53a
    Gitlab: #1187

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -2410,6 +2410,9 @@ CppImpl::slotShowCallView(const std::string& id, lrc::api::conversation::Info or
             return;
     }
 
+    if (!origin.confId.isEmpty())
+        accountInfo_->callModel->setActiveParticipant(origin.confId, origin.callId);
+
     changeView(CURRENT_CALL_VIEW_TYPE, origin);
 }
 
