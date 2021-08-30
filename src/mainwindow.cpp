77e298bf99e32a5bc134885f0a979f8cff8fc3cf
Amin Bandali <amin.bandali@savoirfairelinux.com>
2021-08-30 13:30:22 -0400

    mainwindow: set urgency and indicator status for new interactions
    Set urgency hint for mainwindow and set the indicator's status to
    APP_INDICATOR_STATUS_ATTENTION when there are unread messages,
    pending contact requests, or an incoming new call, to help draw
    user's attention to Jami in these situations.
    
    GitLab: #998
    GitLab: #1233
    Change-Id: I1fda34062cdf079859083f713c73d62544a4ff1b

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -128,6 +128,7 @@ struct MainWindowPrivate
 
     GSettings *window_settings;
     bool useDarkTheme {false};
+    bool is_urgent {false};
 
     details::CppImpl* cpp; ///< Non-UI and C++ only code
 
@@ -153,6 +154,13 @@ struct MainWindowPrivate
 G_DEFINE_TYPE_WITH_PRIVATE(MainWindow, main_window, GTK_TYPE_APPLICATION_WINDOW);
 
 #define MAIN_WINDOW_GET_PRIVATE(obj) (G_TYPE_INSTANCE_GET_PRIVATE ((obj), MAIN_WINDOW_TYPE, MainWindowPrivate))
+
+/* signals */
+enum {
+    URGENCY_CHANGED,
+    LAST_SIGNAL
+};
+static guint main_window_signals[LAST_SIGNAL] = { 0 };
 
 //==============================================================================
 
@@ -330,6 +338,7 @@ public:
     void leaveAccountCreationWizard();
     void enterSettingsView();
     void leaveSettingsView();
+    void updateUrgency();
 
     std::string getCurrentUid();
     void forCurrentConversation(const std::function<void(const lrc::api::conversation::Info&)>& func);
@@ -377,6 +386,8 @@ public:
     QMetaObject::Connection filterChangedConnection_;
     QMetaObject::Connection newConversationConnection_;
     QMetaObject::Connection conversationRemovedConnection_;
+    QMetaObject::Connection interactionStatusUpdatedConnection_;
+    QMetaObject::Connection conversationUpdatedConnection_;
     QMetaObject::Connection accountStatusChangedConnection_;
     QMetaObject::Connection profileUpdatedConnection_;
 
@@ -1524,6 +1535,8 @@ CppImpl::init()
         enterAccountCreationWizard();
     }
 
+    updateUrgency();
+
     widgets->notif_chat_view = g_signal_connect(widgets->notifier, "showChatView",
                                                      G_CALLBACK(on_notification_chat_clicked), self);
     widgets->notif_accept_pending = g_signal_connect(widgets->notifier, "acceptPending",
@@ -1549,6 +1562,8 @@ CppImpl::~CppImpl()
     QObject::disconnect(filterChangedConnection_);
     QObject::disconnect(newConversationConnection_);
     QObject::disconnect(conversationRemovedConnection_);
+    QObject::disconnect(interactionStatusUpdatedConnection_);
+    QObject::disconnect(conversationUpdatedConnection_);
     QObject::disconnect(newAccountConnection_);
     QObject::disconnect(rmAccountConnection_);
     QObject::disconnect(invalidAccountConnection_);
@@ -1984,6 +1999,19 @@ CppImpl::leaveSettingsView()
     }
 }
 
+void
+CppImpl::updateUrgency()
+{
+    auto urgent = lrc_->getAccountModel().notificationsCount() != 0;
+    if (urgent != widgets->is_urgent) {
+        g_signal_emit(G_OBJECT(self),
+                      main_window_signals[URGENCY_CHANGED], 0,
+                      urgent);
+        gtk_window_set_urgency_hint(GTK_WINDOW(self), urgent);
+        widgets->is_urgent = urgent;
+    }
+}
+
 std::string
 CppImpl::getCurrentUid()
 {
@@ -2117,6 +2145,20 @@ CppImpl::updateLrc(const std::string& id, const std::string& accountIdToFlagFree
     newConversationConnection_ = QObject::connect(&*accountInfo_->conversationModel,
                                                   &lrc::api::ConversationModel::newConversation,
                                                   [this] (const QString& id) { slotNewConversation(id.toStdString()); });
+
+    interactionStatusUpdatedConnection_ =
+        QObject::connect(&*accountInfo_->conversationModel,
+                         &lrc::api::ConversationModel::interactionStatusUpdated,
+                         [this] (const QString&, const QString&,
+                                 lrc::api::interaction::Info) {
+                             updateUrgency();
+                         });
+    conversationUpdatedConnection_ =
+        QObject::connect(&*accountInfo_->conversationModel,
+                         &lrc::api::ConversationModel::conversationUpdated,
+                         [this] (const QString&) {
+                             updateUrgency();
+                         });
 
     conversationRemovedConnection_ = QObject::connect(&*accountInfo_->conversationModel,
                                                       &lrc::api::ConversationModel::conversationRemoved,
@@ -2457,6 +2499,8 @@ CppImpl::slotNewIncomingCall(const std::string& accountId, const std::string& co
                             notifId, _("Incoming call"), body, NotificationType::CALL,
                             conv.uid.toStdString());
         }
+
+        updateUrgency();
     }
 
     if (not accountInfo_ or accountInfo_->id.toStdString() != accountId)
@@ -2639,7 +2683,9 @@ void
 CppImpl::slotNewInteraction(const std::string& accountId, const std::string& conversation,
                             const QString& interactionId, const lrc::api::interaction::Info& interaction)
 {
-    if (chatViewConversation_ && chatViewConversation_->get().uid == QString::fromStdString(conversation)) {
+    if (chatViewConversation_
+        && chatViewConversation_->get().uid == QString::fromStdString(conversation)
+        && chatViewConversation_->get().accountId == QString::fromStdString(accountId) ) {
         auto *old_view = gtk_bin_get_child(GTK_BIN(widgets->frame_call));
         if (IS_CURRENT_CALL_VIEW(old_view)) {
             current_call_view_show_chat(CURRENT_CALL_VIEW(old_view));
@@ -2685,6 +2731,8 @@ CppImpl::slotNewInteraction(const std::string& accountId, const std::string& con
                                       avatar.toStdString(), uri.toStdString(), name.toStdString(),
                                       notifId, _("New message"), body.toStdString(), NotificationType::CHAT);
                 }
+
+                updateUrgency();
             }
         }
     } catch (...) {
@@ -2706,6 +2754,7 @@ CppImpl::slotCloseInteraction(const std::string& accountId, const std::string& c
     } catch (...) {
         g_warning("Can't get account %s", accountId.c_str());
     }
+    updateUrgency();
 }
 
 void
@@ -2757,6 +2806,15 @@ main_window_search(MainWindow *win)
     g_return_if_fail(IS_MAIN_WINDOW(win));
     auto *priv = MAIN_WINDOW_GET_PRIVATE(MAIN_WINDOW(win));
     gtk_widget_grab_focus(GTK_WIDGET(priv->search_entry));
+}
+
+bool
+main_window_get_urgency(MainWindow *win)
+{
+    g_return_val_if_fail(IS_MAIN_WINDOW(win), false);
+    auto *priv = MAIN_WINDOW_GET_PRIVATE(MAIN_WINDOW(win));
+    g_return_val_if_fail(priv, false);
+    return priv->is_urgent;
 }
 
 void
@@ -3054,6 +3112,15 @@ main_window_class_init(MainWindowClass *klass)
     gtk_widget_class_bind_template_child_private(GTK_WIDGET_CLASS (klass), MainWindow, scrolled_window_contact_requests);
     gtk_widget_class_bind_template_child_private(GTK_WIDGET_CLASS (klass), MainWindow, image_contact_requests_list);
     gtk_widget_class_bind_template_child_private(GTK_WIDGET_CLASS (klass), MainWindow, image_conversations_list);
+
+    /* add signals */
+    main_window_signals[URGENCY_CHANGED] = g_signal_new(
+        "main-window-urgency-changed",
+        G_TYPE_FROM_CLASS(klass),
+        (GSignalFlags) (G_SIGNAL_RUN_FIRST | G_SIGNAL_ACTION),
+        0, nullptr, nullptr,
+        g_cclosure_marshal_VOID__BOOLEAN,
+        G_TYPE_NONE, 1, G_TYPE_BOOLEAN);
 }
 
 GtkWidget *
